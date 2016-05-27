17b86355b1a51248cb1c47330bbbfc24a7971737
mennadarwish <menna.darwish1@gmail.com>
2016-05-27 18:56:59 +0200

    added an endpoint for the root

--- FAQ_Backend/main/urls.py
+++ FAQ_Backend/main/urls.py
@@ -3,6 +3,7 @@ from main import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
+    url(r'^$', views.api_root),
     url(r'^questions/$', views.QuestionList.as_view(), name="question-list"),
     url(r'^questions/(?P<pk>[0-9]+)/$', views.QuestionDetail.as_view(),
         name="question-detail"),
--- FAQ_Backend/main/views.py
+++ FAQ_Backend/main/views.py
@@ -1,8 +1,19 @@
 from main.models import Question, Tag
 from rest_framework import generics
 from main.serializers import QuestionSerializer, TagSerializer
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
 # from rest_framework import permissions
 # Create your views here.
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'questions': reverse('question-list', request=request, format=format),
+        'tags': reverse('tag-list', request=request, format=format)
+    })
 
 
 class QuestionList(generics.ListCreateAPIView):
