f71ff60cf1f70753fdf735667a3fc154c4b81b0f
mennadarwish <menna.darwish1@gmail.com>
2016-05-27 18:46:03 +0200

    added login to the browsable API

--- FAQ_Backend/FAQ_Backend/urls.py
+++ FAQ_Backend/FAQ_Backend/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('main.urls'))
+    url(r'^', include('main.urls')),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework'))
 ]
