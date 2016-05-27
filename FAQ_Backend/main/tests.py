4848a55153b1b597e28ca246b7c8d547158dab6a
mennadarwish <menna.darwish1@gmail.com>
2016-05-27 21:26:01 +0200

    added Tags tests

--- FAQ_Backend/main/tests.py
+++ FAQ_Backend/main/tests.py
@@ -1,9 +1,9 @@
 from rest_framework.test import APITestCase
-from main.models import Question
+from main.models import Question, Tag
 from django.core.urlresolvers import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test.client import RequestFactory
-from main.views import QuestionDetail
+from main.views import QuestionDetail, TagDetail
 # Create your tests here.
 
 
@@ -30,9 +30,9 @@ class QuestionAPITestCase(APITestCase):
         """
         response = self.client.get('/questions/')
         self.assertEqual(response.status_code, 200)
+        print "response.data", response.data
         self.assertEqual(response.data[0]['body'],
                          'Question1 body')
-        print "response.data", response.data[1]
         self.assertEqual(response.data[1]['title'],
                          'Question2 title')
 
@@ -56,4 +56,45 @@ class QuestionAPITestCase(APITestCase):
         request = self.factory.get(
             reverse('question-detail', kwargs={"pk": 1}))
         response = QuestionDetail.as_view()(request, pk=1)
+        self.assertEqual(response.status_code, 200)
+
+
+class TagAPITestCase(APITestCase):
+    def setUp(self):
+        self.question1 = Question.objects.create(body="Question1 body",
+                                                 title="Question1 title")
+        self.tag = Tag.objects.create(question=self.question1, name="django")
+        self.factory = RequestFactory()
+
+    def test_unicode_representation(self):
+        """
+        Ensures that the model string representation is equal to 'title'
+        """
+        self.assertEqual(str(self.tag), 'django')
+
+    def test_list_tags(self):
+        """
+        Test that we can get a list of questions
+        """
+        response = self.client.get('/tags/')
+        self.assertEqual(response.status_code, 200)
+        print "response.data", response.data
+        self.assertEqual(response.data[0]['name'],
+                         'django')
+
+    def test_create_tag(self):
+        """
+        Test to ensure a tags is created
+        """
+        response = self.client.post(reverse('tag-list'), {
+            u"question": self.question1.pk,
+            u"name": "django",
+        })
+        print "response", response
+        self.assertEqual(response.status_code, 201)
+
+    def test_detail_view_tag(self):
+        request = self.factory.get(
+            reverse('tag-detail', kwargs={"pk": 1}))
+        response = TagDetail.as_view()(request, pk=1)
         self.assertEqual(response.status_code, 200)
