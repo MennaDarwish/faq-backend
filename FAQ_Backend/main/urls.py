665d0cc390b06cf1dd5c7d68d29d617c2f5c9166
mennadarwish <menna.darwish1@gmail.com>
2016-05-27 18:45:09 +0200

    added tags list and detail url

--- FAQ_Backend/main/urls.py
+++ FAQ_Backend/main/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     url(r'^questions/$', views.QuestionList.as_view(), name="question-list"),
     url(r'^questions/(?P<pk>[0-9]+)/$', views.QuestionDetail.as_view(),
         name="question-detail"),
+    url(r'^tags/$', views.TagList.as_view(), name="tag-list"),
+    url(r'^tags/(?P<pk>[0-9]+)/$', views.TagDetail.as_view(),
+        name="tag-detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
