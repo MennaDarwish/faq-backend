aa77ed53f11a7ef928b99165117c8f0af55cb398
mennadarwish <menna.darwish1@gmail.com>
2016-05-27 17:56:08 +0200

    added name to the urls

--- FAQ_Backend/main/urls.py
+++ FAQ_Backend/main/urls.py
@@ -3,8 +3,9 @@ from main import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^questions/$', views.QuestionList.as_view()),
-    url(r'^questions/(?P<pk>[0-9]+)/$', views.QuestionDetail.as_view()),
+    url(r'^questions/$', views.QuestionList.as_view(), name="question-list"),
+    url(r'^questions/(?P<pk>[0-9]+)/$', views.QuestionDetail.as_view(),
+        name="question-detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
